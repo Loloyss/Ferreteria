23abaa77eab7c5b2f881dd0a7b3cb623b38cbdf6
Francisco Álvarez Bellón <122791264+FranDaniels@users.noreply.github.com>
2023-04-09 17:41:15 +0200

    Test select

--- Ferreteria/src/test/kotlin/MainKtTest.kt
+++ Ferreteria/src/test/kotlin/MainKtTest.kt
@@ -1,16 +1,16 @@
+import Articulos.Articulos
+import Articulos.ArticulosDAOImpl
+import com.mysql.cj.jdbc.exceptions.MySQLQueryInterruptedException
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
 
 class MainKtTest {
 
     @org.junit.jupiter.api.Test
+    @DisplayName("select articulos")
     fun main() {
-    }
-
-    @org.junit.jupiter.api.Test
-    fun elecion() {
-    }
-
-    @org.junit.jupiter.api.Test
-    fun buscar() {
+        var articulos=ArticulosDAOImpl()
+        var result=articulos.getArticuloByID("HO","003")
+        assertNotEquals(Articulos("HO","003","Baston del vacio",5.5F,0.0F),result)
     }
 }
