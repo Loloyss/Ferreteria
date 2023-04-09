4de16942900d425f02bae23669b4a4460a91427b
Francisco Álvarez Bellón <122791264+FranDaniels@users.noreply.github.com>
2023-04-09 17:18:53 +0200

    Creacion de los test

---
+++ Ferreteria/src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions.*
+
+class MainKtTest {
+
+    @org.junit.jupiter.api.Test
+    fun main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun elecion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun buscar() {
+    }
+}
